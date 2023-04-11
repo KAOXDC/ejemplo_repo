90a95ca15456a3aeef5055dbb8017baf992029c2
Diego Prado <kaoxdc@gmail.com>
2023-04-11 08:04:49 -0500

    se agrego la tabla marca y categoria

--- script_tienda.sql
+++ script_tienda.sql
@@ -4,3 +4,13 @@ nombre_producto VARCHAR(50),
 id_marca INT,
 FOREIGN KEY (id_marca) REFERENCES marca(id_marca)
 );
+
+CREATE TABLE marca (
+id_marca INT PRIMARY KEY,
+nombre_marca VARCHAR(50)
+);
+
+CREATE TABLE categoria (
+id_categoria INT PRIMARY KEY,
+nombre_categoria VARCHAR(50)
+);
